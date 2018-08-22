4c569770b024df908f817e820190cd8c340e8144
tshita <shiitada@gmail.com>
2018-08-22 11:27:33 +0900

    add template directory

---
+++ template/algorithm_template.cc
@@ -0,0 +1,34 @@
+/*
+  Algorithm Name (Author)
+  =======================
+
+  # Algorithm
+    Input:
+    Output:
+
+  # Complexity
+
+  # Usage
+
+  # Description
+
+  # Note
+
+  # References
+
+  # Verified
+
+*/
+
+#include <bits/stdc++.h> // Replace needed headers for speed up
+
+using namespace std;
+
+// -------------8<------- start of library -------8<------------------------
+
+// -------------8<------- end of library ---------8-------------------------
+int main() {
+    cin.tie(0); ios::sync_with_stdio(false);
+
+    return 0;
+}
---
+++ template/template.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const double PI = acos(-1);
+
+inline void INITIOS() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // cout << setw(4); // printf("%4d", x);
+    // cout << setw(4) << setfill('0'); // printf("%04d", x);
+    // cout << setprecision(4) << setiosflags(ios::fixed); // printf("%.4f", x);
+    // cout << hex << nouppercase; // printf("%x", x);
+}
+
+
+int main() {
+    INITIOS();
+
+    
+
+    return 0;
+}
