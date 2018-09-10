b35aa524f8f3e04c9cc678bc8772ce931dc8f1b0
tshita <shiitada@gmail.com>
2018-09-10 09:46:02 +0900

    gcdとlcmを追加

---
+++ number_theory/gcd_lcm.cc
@@ -0,0 +1,49 @@
+/*
+  最大公約数・最小公倍数
+  =======================
+
+  # Problem
+    最大公約数(GCD; greatest common divisor)
+      Input: 整数 a, b
+      Output: max_{整数 c, a | c and b | c} (c)
+
+    最小公倍数(LCM; least common multiple)
+      Input: 整数 a, b
+      Output: min_{整数 c, c | a and c | a} (c)
+
+  # Complexity
+    Time : O(log(min(a, b)))
+
+  # Algorithm
+    整数 a, b (a >= b) の最大公約数 gcd(a, b) はユークリッドの互除法，
+     gcd(a, b) = gcd(b, a % b)
+    を再帰的に計算している．最小公倍数 lcm(a, b) は，
+     ab = gcd(a, b) * lcm(a, b)
+    を最大公約数を用いて計算している．
+
+  # Note
+    - C++17からSTLに追加. std::gcd(a, b) と std::lcm(a, b)
+　　 - GCC拡張に最大公約数 __gcd(a, b) がある
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// -------------8<------- start of library -------8<------------------------
+using ll = long long;
+
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+// -------------8<------- end of library ---------8-------------------------
+
+int main() {
+    ll a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
+    cout << lcm(a, b) << endl;
+
+    return 0;
+}
