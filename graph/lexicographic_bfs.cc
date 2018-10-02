d7bb0f01fdd3a74f560d54a96deaada5dd97b99c
tshita <shiitada@gmail.com>
2018-10-02 12:28:32 +0900

    メモリリークを修正

--- graph/lexicographic_bfs.cc
+++ graph/lexicographic_bfs.cc
@@ -11,15 +11,21 @@
     Time and Space: O(|V| + |E|)
 
   # Usage
-    vector<int> LexBfs(vector<vector<int>> G)
+    - vector<int> LexBfs(vector<vector<int>> G)
+    - 戻り値 vector<int> ord : ord[i] = v は i番目(0-index)に訪れた頂点番号(0-index)
 
   # Description
     G の Lexicographic Bradth First Search(LexBfs) とは BFS の一種である．
-    ある頂点を始点としてBFSを行うが，次に訪れる頂点は，未訪問の頂点で，その頂点に隣接している
+    任意の頂点を始点としてBFSを行うが，次に訪れる頂点は，未訪問の頂点で，その頂点に隣接している
     訪問済みの頂点らの訪れた順番の辞書式順序が最も小さいものが選ばれる．
     例えば，未訪問の頂点が2つあり，それぞれの隣接している訪問済み頂点の訪れた順番が 1,3,4 と
     1,3,6 のときは前者が次に選ばれる．
     線形時間で実行するために Partition Refinement を実装した．
+
+  # Note
+    - スマートポインタ(std::shared_ptr)を使うと遅くなるけど使用
+    - 何故か遅いので調査(|V| = 10000, |E| = 30000000 ぐらいで 6.5 [s])
+      -> 多分定数倍が重い
 
   # References
     Derek G. Corneil : Lexicographic Breadth First Search -- A Survey.
@@ -32,6 +38,7 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using Graph = std::vector<std::vector<int>>;
 
@@ -39,7 +46,7 @@ using Graph = std::vector<std::vector<int>>;
 std::vector<int> LexBfs(const Graph &g) {
     struct Data {
         Data(int _s) : prev(nullptr), nxt(nullptr), size(_s), size_new(0), item(_s) {}
-        Data *prev, *nxt;
+        std::shared_ptr<Data> prev, nxt;
         int size, size_new;
         std::vector<int> item;
     };
@@ -47,14 +54,14 @@ std::vector<int> LexBfs(const Graph &g) {
     const int n = g.size();
     std::vector<int> order(n, -1);
     std::vector<char> visited(n, false); // vector<bool> is slow
-    std::vector<std::pair<Data*, int>> ptr(n);
-    Data data(n);
+    std::vector<std::pair<std::shared_ptr<Data>, int>> ptr(n);
+    std::shared_ptr<Data> data = std::make_shared<Data>(n);
     for (int i = 0;  i < n; ++i) {
-        ptr[i] = std::make_pair(&data, i);
-        data.item[i] = i;
+        ptr[i] = std::make_pair(data, i);
+        data->item[i] = i;
     }
 
-    Data *head = &data;
+    std::shared_ptr<Data> head(data);
     int size_order = 0;
     while (size_order < n) {
         // choosing a pivot
@@ -69,7 +76,7 @@ std::vector<int> LexBfs(const Graph &g) {
         for (const auto u : g[pivot]) {
             if (visited[u]) continue;
 
-            Data *cur = ptr[u].first;
+            std::shared_ptr<Data> cur(ptr[u].first);
             if (cur->size == 1) continue;
 
             const int idx = ptr[u].second;
@@ -82,14 +89,14 @@ std::vector<int> LexBfs(const Graph &g) {
         for (const auto u : g[pivot]) {
             if (visited[u]) continue;
 
-            Data *cur = ptr[u].first;
+            std::shared_ptr<Data> cur(ptr[u].first);
             if (cur->size_new == 0 || cur->size == cur->size_new) {
                 cur->size_new = 0;
                 continue;
             }
 
             const int size_new = cur->size_new;
-            Data *new_data = new Data(size_new);
+            std::shared_ptr<Data> new_data = std::make_shared<Data>(size_new);
             for (int i = 0; i < size_new; ++i) {
                 new_data->item[i] = cur->item[cur->size - size_new + i];
                 ptr[new_data->item[i]].first = new_data;
@@ -113,6 +120,8 @@ std::vector<int> LexBfs(const Graph &g) {
 
 
 int main() {
+    std::cin.tie(0); std::ios::sync_with_stdio(false);
+
     // Input Undirected Graph G = (V, E)
     // n := |V|, m := |E|
     int n, m, v[2];
