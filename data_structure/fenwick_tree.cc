86df5c6dfff7e50ef3c1c5b0d532f3a7f26aff8a
shiitada <shiitada@gmail.com>
2019-01-09 13:37:43 +0900

    add: 初期化のメソッドを追加

--- data_structure/fenwick_tree.cc
+++ data_structure/fenwick_tree.cc
@@ -10,7 +10,14 @@
       2. v_0 + v_1 + ... + v_{i-1} を求める　: O(log n) 時間
 
   # Usage
-    - FenwickTree<T> tree(n) : 型 T でサイズ n の Fenwick Tree を作成 (bit[i] = 0)
+    - FenwickTree<T> tree: 型 T の空の Fenwick Tree を作成（）
+    - FenwickTree<T> tree(n): 型 T でサイズ n の Fenwick Tree を作成 (bit[i] = 0)
+
+    - tree.resize(n): tree のサイズを n に変更して，全ての要素を 0 に初期化
+    - tree.init(T w): tree の要素を全て w で初期化
+    - tree.init(T w, n): tree のサイズを n に変更して，要素を全て w で初期化
+    - tree.init(vector<int> w): tree のサイズを w のサイズに変更して，w の要素で初期化
+
     - T tree.sum(r) : bit[0] + bit[1] + ... + bit[r - 1] を
     - T tree.sum(l, r) : bit[l] + bit[l+1] + ... + bit[r - 1] を返す
     - void tree.add(i, a) : bit[i] += a
@@ -18,35 +25,50 @@
 
   # Note
     Binary Indexed Tree とも呼ばれる
-    ここでは，1-idx よりも 0-idx でも実装を行っている
+    ここでは 0-idx で実装を行っている
     平衡二分探索木やSegment Treeよりも定数倍の意味で高速（bit演算と少ない領域）
 
   # References
     [保坂和宏，Binary Indexed Treeのはなし（2018年8月26日アクセス）]
-    (http://hos.ac/slides/20140319_bit.pdf)
+    (http://hos.ac/slides/20140319_bit.pdf )
 
   # Verified
     [AOJ DSL-DSL_2_B ``Range Query'']
-    (http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp)
+    (http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp )
 */
 
-#include <bits/stdc++.h> // Replace needed headers for speed up
+#include <iostream>
+#include <vector>
 
 // -------------8<------- start of library -------8<------------------------
 template <class T>
 struct FenwickTree {
-    const int n;
+    size_t n;
     std::vector<T> bit;
 
-    FenwickTree(int _n = 0) : n(_n), bit(n + 1, 0) {}
+    FenwickTree() : n(0) {}
+    FenwickTree(int _n) : n(_n), bit(n, 0) {}
+
+    void resize(const int _n) { n = _n; bit.resize(n, 0); }
+    void init(const T &w, size_t _n = 0) {
+        if (_n != 0) { n = _n; resize(_n); }
+        for (size_t i = 0; i < n; ++i) bit[i] = w;
+        setup();
+    }
+    void init(const std::vector<T> &w) {
+        if (w.size() != n) resize(w.size());
+        for (size_t i = 0; i < n; ++i) bit[i] = w[i];
+        setup();
+    }
+    void setup() { for (size_t i = 0; i < n; ++i) bit[i | (i + 1)] += bit[i]; }
 
     T sum(int r) const {
         T res = 0;
         for (r = r - 1; r >= 0; r = (r & (r + 1)) - 1) res += bit[r];
         return res;
     }
-    T sum(int l, int r) const { return sum(r) - sum(l); }
-    void add(int idx, const T &w) { for (; idx < n; idx |= idx + 1) bit[idx] += w; }
+    T sum(const int l, const int r) const { return sum(r) - sum(l); }
+    void add(size_t idx, const T &w) { for (; idx < n; idx |= idx + 1) bit[idx] += w; }
     int size() const { return n; }
 };
 // -------------8<------- end of library ---------8-------------------------
