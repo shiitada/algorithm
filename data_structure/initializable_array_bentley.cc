93293b357c904d28b5e070d5bc3fcd7878882254
shiitada <shiitada@gmail.com>
2019-04-25 02:59:56 +0900

    説明の誤りを変更

--- data_structure/initializable_array_bentley.cc
+++ data_structure/initializable_array_bentley.cc
@@ -11,24 +11,35 @@
     - set(i, a): x[i] に a を代入 [Time O(1)]
     - x[i]: x[i] を返す [Time O(1)]
     - init(a): x のすべての要素に a を代入 [Time O(1)]
+    - size(): 配列の要素数を返す [Time O(1)]
+    - resize(n): 要素数を n に変更（前の要素は全てクリアされる）[Time O(n)]
 
   # Description
     init(a) を実現するために領域を (2 * n + 2) * sizeof(size_t) + sizeof(T) だけ追加している．
-    上の操作を実現する抽象配列を Z とする．initv は最近行われた init(a) の a の値で，
-    init(a) を from と スタック to（スタックサイズ b）を用いたチェインという概念で実現する．
+    上の操作を実現する抽象配列を Z とする．T型変数 initv は最近行われた初期化操作 init(a) の a の値を表す．
+    Z は要素数 n の配列 from と スタック to（スタックサイズ b，初期サイズ 0）を用いたチェインという概念で実現する．
     Z[i] は i 番目がチェインのとき value[i] で，チェインでないとき initv とする．
-    i 番目がチェインであるとは，from[i] がスタックに push されており (from[i] < b)，
-    かつ，from[i] と to がリンクしていることである（to[from[i]] = i）．
+    すなわち，i 番目がチェインであるとは，i 番目の要素が直近の初期化後に代入されたかどうかを表す．
+
+    i 番目がチェインであるとは，値 i がスタック to に push されており，from[i] と to がリンクしていることである．
+    from[i] と to がリンクしているとは，値 i が to の from[i] 番目に push されていることを意味する．
+    すなわち，to[from[i]] = i を満たすことである．また，i が to に push されていることから
+    to のスタックサイズ b に対しても from[i] < b を満たす．
 
     init(a) はスタックを空にして（b = 0），initv = a とすればよい．
-    set(i, a) は i 番目がチェインのときは value[i] = a として，チェインでないときはスタックに
-    from[i] を追加して to とリンクしてから value[i] = a とする．
+    set(i, a) は i 番目がチェインのときは value[i] = a として，
+    チェインでないときはスタックに i を追加して，from[i] と to をリンクしてから value[i] = a とする．
     x[i] も i　番目がチェインであるかどうかで value[i] を返すか initv を返すかを行えばよい．
     これらは，すべて O(1) 時間で実現可能である．
 
   # Note
+    配列外参照のチェックは行っていない（必要なら入れる）
+
     @tmaehara さんの実装を vector にしただけ・・・．TODO としてどっちが高速なのかの実験を行う．
     @kgoto さんのブログに追加領域をより少なくできる論文があると書かれているのでいつか実装する．
+
+    アルゴリズムイントロダクションの演習問題11.1-4にある，直接アドレス表が巨大な配列のときに探索，挿入，削除を
+    O(1)時間で行えという問題に対して，直接アドレス表をこのデータ構造にすることで実現できる．
 
   # References
     - [spagetthi source @tmaehara]
@@ -39,20 +50,29 @@
 
 */
 
-#include <bits/stdc++.h> // Replace needed headers for speed up
+#include <iostream>
+#include <vector>
 
 // -------------8<------- start of library -------8<------------------------
 template <class T>
-struct InitializableArray {
+class InitializableArray {
     T initv;
-    size_t n, b = 0;
+    size_t n = 0, b = 0;
     std::vector<T> value;
     std::vector<size_t> from, to;
 
+    bool chain(const size_t i) const { return from[i] < b && to[from[i]] == i; }
+
+public:
     explicit InitializableArray(int _n) : n(_n), value(n), from(n), to(n) {}
 
-    bool chain(const size_t i) const { return from[i] < b && to[from[i]] == i; }
     T operator[](const size_t i) const { return chain(i) ? value[i] : initv; }
+    size_t size() const { return n; }
+    void resize(const size_t _n) {
+        n = _n; b = 0;
+        value.resize(n); from.resize(n); to.resize(n);
+    }
+
     void init(const T &a) { initv = a; b = 0; }
     void set(const size_t i, const T &a) {
         if (!chain(i)) { from[i] = b; to[b++] = i; }
